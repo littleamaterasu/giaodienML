f71bf396de7f760d468d33a1e15131d6ffe06396
littleamaterasu <clgtthangtung@gmail.com>
2024-01-06 01:07:30 +0700

    a

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-import flask
-from flask import Flask, render_template, request, redirect
-import cv2
-import numpy as np
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join('static', 'results')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('upload.html')
+
+
+name_list = []
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'files[]' not in request.files:
+        return redirect(request.url)
+
+    files = request.files.getlist('files[]')
+
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            if filename not in name_list:
+                name_list.append(filename)
+
+    return render_template('upload.html', name_list=name_list)
+
+
+@app.route('/results', methods=['GET'])
+def show_image():
+    return render_template('results.html', name_list=name_list)
 
 
 if __name__ == '__main__':
